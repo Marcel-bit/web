26505a7904a159be7fab62331096eaeeec97fc44
Marcel-bit <49585259+Marcel-bit@users.noreply.github.com>
2024-12-04 11:42:21 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import requests
+from tinydb import TinyDB, Query
+
+db = TinyDB('db.json')
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    print(request.remote_addr)
+    klic = requests.get("https://api.chucknorris.io/jokes/random").json()
+    tole = (klic["value"])
+
+    db.insert({'vic': tole})
+    return render_template("index.html", spremenljivka = tole, elements = list(range(10)))
+
+
+@app.route("/data")
+def test():
+    klic = requests.get("https://api.chucknorris.io/jokes/random").json()
+    return klic["value"]
+    
+
+
+app.run(debug=True, port=8080)
